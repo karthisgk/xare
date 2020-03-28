655954b036b63c03891a4b1466bd48c1a4011c29
karthisgk <karthisg.sg@gmail.com>
2020-03-28 13:12:45 +0530

    sdfd

--- src/js/const.js
+++ src/js/const.js
@@ -1,8 +1,8 @@
-const PRODUCTION = true;
+const PRODUCTION = false;
 module.exports = {
     PRODUCTION,
     dbName: 'xare',
-    dbUrl: PRODUCTION ? 'mongodb://localhost' : 'mongodb://me.karthisgk.be',
-    liveUrl: PRODUCTION ? 'http://me.karthisgk.be:4040/' : 'http://192.168.43.97:4040/',
+    dbUrl: PRODUCTION ? 'mongodb://localhost' : 'mongodb://localhost',
+    liveUrl: PRODUCTION ? 'http://me.karthisgk.be:4040/' : 'http://me.karthisgk.be:4040/',
     defaultIconName: 'default.png'
 }
